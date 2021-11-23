85146016d31b0a0e9c77f11630b184c9972bb303
jbock <jbock-java@gmx.de>
2021-11-23 19:59:33 +0100

    Migrate TruthFailureSubjectTest to JUnit 5
    closes #22

--- src/test/java/com/google/common/truth/TruthFailureSubjectTest.java
+++ src/test/java/com/google/common/truth/TruthFailureSubjectTest.java
@@ -17,35 +17,38 @@
 package com.google.common.truth;
 
 import com.google.common.collect.ImmutableList;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Fact.fact;
 import static com.google.common.truth.Fact.simpleFact;
 import static com.google.common.truth.TruthFailureSubject.HOW_TO_TEST_KEYS_WITHOUT_VALUES;
 import static com.google.common.truth.TruthFailureSubject.truthFailures;
 import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /** Tests for {@link TruthFailureSubject}. */
-@RunWith(JUnit4.class)
-public class TruthFailureSubjectTest extends BaseSubjectTestCase {
+class TruthFailureSubjectTest extends BaseSubjectTestCase {
     // factKeys()
 
     @Test
-    public void factKeys() {
+    void factKeys() {
         assertThat(fact("foo", "the foo")).factKeys().containsExactly("foo");
     }
 
     @Test
-    public void factKeysNoValue() {
+    void factKeysNoValue() {
         assertThat(simpleFact("foo")).factKeys().containsExactly("foo");
     }
 
     @Test
-    public void factKeysFail() {
-        expectFailureWhenTestingThat(fact("foo", "the foo")).factKeys().containsExactly("bar");
-        Truth.assertThat(expectFailure.getFailure())
+    void factKeysFail() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> Truth.assertAbout(truthFailures())
+                        .that(failure(fact("foo", "the foo")))
+                        .factKeys()
+                        .containsExactly("bar"));
+        Truth.assertThat(failure)
                 .hasMessageThat()
                 .contains("value of: failure.factKeys()");
         // TODO(cpovirk): Switch to using fact-based assertions once IterableSubject uses them.
@@ -54,61 +57,90 @@ public class TruthFailureSubjectTest extends BaseSubjectTestCase {
     // factValue(String)
 
     @Test
-    public void factValue() {
+    void factValue() {
         assertThat(fact("foo", "the foo")).factValue("foo").isEqualTo("the foo");
     }
 
     @Test
-    public void factValueFailWrongValue() {
-        expectFailureWhenTestingThat(fact("foo", "the foo")).factValue("foo").isEqualTo("the bar");
-        assertFailureValue("value of", "failure.factValue(foo)");
+    void factValueFailWrongValue() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> assertThat(fact("foo", "the foo"))
+                        .factValue("foo")
+                        .isEqualTo("the bar"));
+        assertFailureValue(failure,
+                "value of", "failure.factValue(foo)");
     }
 
     @Test
-    public void factValueFailNoSuchKey() {
-        Object unused = expectFailureWhenTestingThat(fact("foo", "the foo")).factValue("bar");
-        assertFailureKeys("expected to contain fact", "but contained only");
-        assertFailureValue("expected to contain fact", "bar");
-        assertFailureValue("but contained only", "[foo]");
-    }
-
-    @Test
-    public void factValueFailMultipleKeys() {
-        Object unused =
-                expectFailureWhenTestingThat(fact("foo", "the foo"), fact("foo", "the other foo"))
-                        .factValue("foo");
-        assertFailureKeys("expected to contain a single fact with key", "but contained multiple");
-        assertFailureValue("expected to contain a single fact with key", "foo");
-        assertFailureValue("but contained multiple", "[foo: the foo, foo: the other foo]");
-    }
-
-    @Test
-    public void factValueFailNoValue() {
-        Object unused = expectFailureWhenTestingThat(simpleFact("foo")).factValue("foo");
+    void factValueFailNoSuchKey() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> assertThat(fact("foo", "the foo"))
+                        .factValue("bar"));
         assertFailureKeys(
+                failure,
+                "expected to contain fact", "but contained only");
+        assertFailureValue(
+                failure,
+                "expected to contain fact", "bar");
+        assertFailureValue(
+                failure,
+                "but contained only", "[foo]");
+    }
+
+    @Test
+    void factValueFailMultipleKeys() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> Truth.assertAbout(truthFailures())
+                        .that(failure(fact("foo", "the foo"), fact("foo", "the other foo")))
+                        .factValue("foo"));
+        assertFailureKeys(
+                failure,
+                "expected to contain a single fact with key", "but contained multiple");
+        assertFailureValue(
+                failure,
+                "expected to contain a single fact with key", "foo");
+        assertFailureValue(
+                failure,
+                "but contained multiple", "[foo: the foo, foo: the other foo]");
+    }
+
+    @Test
+    void factValueFailNoValue() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> Truth.assertAbout(truthFailures())
+                        .that(failure(simpleFact("foo")))
+                        .factValue("foo"));
+        assertFailureKeys(
+                failure,
                 "expected to have a value",
                 "for key",
                 "but the key was present with no value",
                 HOW_TO_TEST_KEYS_WITHOUT_VALUES.key);
-        assertFailureValue("for key", "foo");
+        assertFailureValue(
+                failure,
+                "for key", "foo");
     }
 
     // factValue(String, int)
 
     @Test
-    public void factValueInt() {
+    void factValueInt() {
         assertThat(fact("foo", "the foo")).factValue("foo", 0).isEqualTo("the foo");
     }
 
     @Test
-    public void factValueIntMultipleKeys() {
+    void factValueIntMultipleKeys() {
         assertThat(fact("foo", "the foo"), fact("foo", "the other foo"))
                 .factValue("foo", 1)
                 .isEqualTo("the other foo");
     }
 
     @Test
-    public void factValueIntFailNegative() {
+    void factValueIntFailNegative() {
         try {
             assertThat(fact("foo", "the foo")).factValue("foo", -1);
             fail();
@@ -117,53 +149,103 @@ public class TruthFailureSubjectTest extends BaseSubjectTestCase {
     }
 
     @Test
-    public void factValueIntFailWrongValue() {
-        expectFailureWhenTestingThat(fact("foo", "the foo")).factValue("foo", 0).isEqualTo("the bar");
-        assertFailureValue("value of", "failure.factValue(foo, 0)");
+    void factValueIntFailWrongValue() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> Truth.assertAbout(truthFailures())
+                        .that(failure(fact("foo", "the foo")))
+                        .factValue("foo", 0)
+                        .isEqualTo("the bar"));
+        assertFailureValue(
+                failure,
+                "value of", "failure.factValue(foo, 0)");
     }
 
     @Test
-    public void factValueIntFailNoSuchKey() {
-        Object unused = expectFailureWhenTestingThat(fact("foo", "the foo")).factValue("bar", 0);
-        assertFailureKeys("expected to contain fact", "but contained only");
-        assertFailureValue("expected to contain fact", "bar");
-        assertFailureValue("but contained only", "[foo]");
-    }
-
-    @Test
-    public void factValueIntFailNotEnoughWithKey() {
-        Object unused = expectFailureWhenTestingThat(fact("foo", "the foo")).factValue("foo", 5);
-        assertFailureKeys("for key", "index too high", "fact count was");
-        assertFailureValue("for key", "foo");
-        assertFailureValue("index too high", "5");
-        assertFailureValue("fact count was", "1");
-    }
-
-    @Test
-    public void factValueIntFailNoValue() {
-        Object unused = expectFailureWhenTestingThat(simpleFact("foo")).factValue("foo", 0);
+    void factValueIntFailNoSuchKey() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> Truth.assertAbout(truthFailures())
+                        .that(failure(fact("foo", "the foo")))
+                        .factValue("bar", 0));
         assertFailureKeys(
+                failure,
+                "expected to contain fact", "but contained only");
+        assertFailureValue(
+                failure,
+                "expected to contain fact", "bar");
+        assertFailureValue(
+                failure,
+                "but contained only", "[foo]");
+    }
+
+    @Test
+    void factValueIntFailNotEnoughWithKey() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> Truth.assertAbout(truthFailures())
+                        .that(failure(fact("foo", "the foo")))
+                        .factValue("foo", 5));
+        assertFailureKeys(
+                failure,
+                "for key", "index too high", "fact count was");
+        assertFailureValue(
+                failure,
+                "for key", "foo");
+        assertFailureValue(
+                failure,
+                "index too high", "5");
+        assertFailureValue(
+                failure,
+                "fact count was", "1");
+    }
+
+    @Test
+    void factValueIntFailNoValue() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> Truth.assertAbout(truthFailures())
+                        .that(failure(simpleFact("foo")))
+                        .factValue("foo", 0));
+        assertFailureKeys(
+                failure,
                 "expected to have a value",
                 "for key",
                 "and index",
                 "but the key was present with no value",
                 HOW_TO_TEST_KEYS_WITHOUT_VALUES.key);
-        assertFailureValue("for key", "foo");
-        assertFailureValue("and index", "0");
+        assertFailureValue(
+                failure,
+                "for key", "foo");
+        assertFailureValue(
+                failure,
+                "and index", "0");
     }
 
     // other tests
 
     @Test
-    public void nonTruthErrorFactKeys() {
-        Object unused = expectFailureWhenTestingThat(new AssertionError()).factKeys();
-        assertFailureKeys("expected a failure thrown by Truth's new failure API", "but was");
+    void nonTruthErrorFactKeys() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> Truth.assertAbout(truthFailures())
+                        .that(new AssertionError())
+                        .factKeys());
+        assertFailureKeys(
+                failure,
+                "expected a failure thrown by Truth's new failure API", "but was");
     }
 
     @Test
-    public void nonTruthErrorFactValue() {
-        Object unused = expectFailureWhenTestingThat(new AssertionError()).factValue("foo");
-        assertFailureKeys("expected a failure thrown by Truth's new failure API", "but was");
+    void nonTruthErrorFactValue() {
+        AssertionError failure = assertThrows(
+                AssertionError.class,
+                () -> Truth.assertAbout(truthFailures())
+                        .that(new AssertionError())
+                        .factValue("foo"));
+        assertFailureKeys(
+                failure,
+                "expected a failure thrown by Truth's new failure API", "but was");
     }
 
     private TruthFailureSubject assertThat(Fact... facts) {
